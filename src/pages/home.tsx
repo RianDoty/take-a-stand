5e368ac234a0e4fec7796b57ff3d05b486567a26
Techno <95896107+RianDoty@users.noreply.github.com>
2023-05-23 23:03:10 +0000

    Update 11 files

---
+++ src/pages/home.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+import { Link } from "wouter";
+
+//Home page
+export default function Home() {
+  return (
+    <>
+      <h1 className="title">welcome to the home</h1>
+    </>
+  );
+}
