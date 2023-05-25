c86de16ce08fa09f1381d513c631b542ff386e5c
TechnoDraconic <riandoty@gmail.com>
2023-05-25 08:01:32 -0700

    AAAAA

--- src/components/progress.tsx
+++ src/components/progress.tsx
@@ -1,23 +1,36 @@
-import { Children } from "react";
+import React, { Children, useContext } from "react";
+
+const ProgressContext = React.createContext(0)
+
+export function Exclusive({children, progress}: {children: React.ReactNode, progress: number}) {
+  const currentProgress = useContext(ProgressContext)
+
+  if (currentProgress === progress) {
+    return <>{children}</>
+  }
+  return null
+}
 
 export function ProgressManager({
   progress,
   children,
+  exclusive = false
 }: {
   progress: number;
   children: React.ReactNode;
+  exclusive?: boolean
 }) {
   const childrenArray = Children.toArray(children) as React.ReactElement<
     any,
     string | React.JSXElementConstructor<any>
   >[];
   const newChildren = childrenArray.map((child, index) => {
-    if (index <= progress) {
-      return child;
+    if ((exclusive && index === progress) || (!exclusive && index <= progress)) {
+      return child
     }
   });
 
-  return <>{newChildren}</>;
+  return <ProgressContext.Provider value={progress}>{newChildren}</ProgressContext.Provider>;
 }
 
 export function ProgressGroup({
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,36 +1,53 @@
 import { useState } from "react";
 import { Link } from "wouter";
 import { SimLayout, PovertySim1 } from "../components/povertysim";
-import { ProgressManager, ProgressGroup } from "../components/progress";
+import { ProgressManager, ProgressGroup, Exclusive } from "../components/progress";
 
 //Home page
 export default function Home() {
+  
   const [progress, setProgress] = useState(0);
+
+  function progressMain() {
+    setProgress((p:number) => p+1)
+  }
 
   return (
     <>
       <h1 className="title">welcome to the home</h1>
+      
       <h2>During COVID-19, {"<statistic>"}</h2>
-      <ProgressManager progress={progress}>
-        <SimLayout sim={PovertySim1}>
-          <ProgressGroup>
-            Let's say that you work a minimum wage. As of 2023 in America, that
-            means that you'd make a solid $7.25 for each hour you'd be working.
-            <div className="muted">
-              Psst! Move the "salary" slider to $7.25!{" "}
-            </div>
-          </ProgressGroup>
-          <ProgressGroup>
-            That seems like a pretty good monthly salary! <br />
-            Comparing it to the montly costs, that leaves a pretty good chunk of
-            pocket change.
-          </ProgressGroup>
-          <ProgressGroup>
-            Except... <br />
-            Those aren't numbers from 2023. <br />
-            <i>Those are the numbers from 2009.</i>
-          </ProgressGroup>
-        </SimLayout>
+      <ProgressManager progress={progress} exclusive={true}>
+        <ProgressGroup>
+          <SimLayout sim={PovertySim1}>
+            <ProgressGroup>
+              <Exclusive progress={0}><button>Unlock Everything (for grading)</button></Exclusive>
+              <br/>
+              Let's say that you work at minimum wage. As of 2023 in America, that
+              means that you'd make at least $7.25 for each hour you'd be working.
+              <Exclusive progress={0}><div className="muted">
+                Psst! Move the "salary" slider to $7.25!{" "}
+              </div></Exclusive>
+            </ProgressGroup>
+            <ProgressGroup>
+              That seems like a pretty good monthly salary! <br />
+              Comparing it to the montly costs, that leaves a pretty good chunk of
+              pocket change.
+            </ProgressGroup>
+            <ProgressGroup>
+              Except... <br />
+              Those aren't numbers from 2023. <br />
+              <i>Those are the numbers from 2009.</i>
+              <button onClick={progressMain}>The real situation...</button>
+            </ProgressGroup>
+          </SimLayout>
+        </ProgressGroup>
+
+        <ProgressGroup>
+          ...is much worse.<br/><br/>
+          Taking into account inflation rates from the past 14 years,
+          that $7.25
+        </ProgressGroup>
       </ProgressManager>
     </>
   );
